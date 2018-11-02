a220d6a05ada10b802e5423a5345526da12fbaa3
Gregory Hall <gregoryhall994@gmail.com>
2018-11-02 09:46:40 -0600

    removed CN expectation on group names and changed headers to not require mapping from OIDC

--- server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/security/HeaderRealm.java
+++ server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/security/HeaderRealm.java
@@ -133,34 +133,24 @@ public class HeaderRealm
 			HeaderAuthToken headerAuthToken = new HeaderAuthToken();
 			headerAuthToken.setRequest(request);
 			headerAuthToken.setAdminGroupName(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_ADMIN_GROUP));
-			headerAuthToken.setEmail(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_EMAIL, "mail")));
+			headerAuthToken.setEmail(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_EMAIL, "email")));
 			headerAuthToken.setPhone(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_PHONE, "telephonenumber")));
-			headerAuthToken.setFirstname(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_FIRSTNAME, "givenname")));
+			headerAuthToken.setFirstname(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_FIRSTNAME, "given_name")));
 
+			StringBuffer groups = new StringBuffer();
 			Enumeration<String> groupValues = request.getHeaders(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_GROUP, "memberOf"));
-			StringBuilder group = new StringBuilder();
 			while (groupValues.hasMoreElements()) {
-				String adGroup = groupValues.nextElement();
-				group.append(adGroup);
-				group.append(" | ");
-				
-				//CN=STORE-Admin,OU=Groups,OU=DI2E-F,DC=basef,DC=dev,DC=lab
-				//extract the CN values
-				String groupFragments[] = adGroup.split(",");				
-				for (String fragment : groupFragments) {
-					String keyValue[] = fragment.split("=");
-					if ("CN".equals(keyValue[0].trim())) {
-						headerAuthToken.getGroups().add(keyValue[1]);
-					}
-				}
-				
+				String group = groupValues.nextElement();
+				groups.append(group);
+				groups.append(" | ");
+				headerAuthToken.getGroups().add(group);
 			}
 
-			headerAuthToken.setGroup(group.toString());
+			headerAuthToken.setGroup(groups.toString());
 			headerAuthToken.setGuid(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_LDAPGUID, STUB_HEADER)));
-			headerAuthToken.setLastname(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_LASTNAME, "sn")));
-			headerAuthToken.setOrganization(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_ORGANIZATION, "company")));
-			headerAuthToken.setUsername(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_USERNAME, "sAMAccountName")));
+			headerAuthToken.setLastname(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_LASTNAME, "family_name")));
+			headerAuthToken.setOrganization(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_ORGANIZATION, "organization")));
+			headerAuthToken.setUsername(request.getHeader(PropertiesManager.getInstance().getValue(PropertiesManager.KEY_OPENAM_HEADER_USERNAME, "preferred_username")));
 
 			try {
 				Subject currentUser = SecurityUtils.getSubject();
